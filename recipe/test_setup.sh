dc72d9e5a10f98790358ba8142b8da9ef4f6431c
Sunil Mahawar <sunil.mahawar@intel.com>
2016-09-16 09:45:41 -0700

    Updated recipe files Fixed minor issue in sec4 &sec5 files for installation HPC added additional file for dib/elements/hpc-env-base, was missing in previous checkin. created independent script for updaing sms with cnods

---
+++ recipe/test_setup.sh
@@ -0,0 +1,8 @@
+# get the hostname of sms
+
+# update hostname into input file to recipe, replace sms_name with hostname
+# find network which will be used for provisioning -- manual
+# update into file
+
+# Update /etc/hosts with entry of sms_name
+#  
