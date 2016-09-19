3184853a80d4a71fcc6bdb6d260d21638b1d5b62
Sunil Mahawar <sunil.mahawar@intel.com>
2016-09-19 09:46:27 -0700

    Updated file after local testing

--- recipe/setup_cloud_hpc.sh
+++ recipe/setup_cloud_hpc.sh
@@ -20,14 +20,14 @@ set -E # traps on ERR will now be inherited by shell functions,
        # command substitution or subshells - equivalent to set -o errtrace
 
 #For Debugging 
-set -x
+#set -x
 SCRIPTDIR="$( cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd -P && echo x)"
 SCRIPTDIR="${SCRIPTDIR%x}"
 cd $SCRIPTDIR
 
 pwd
 # enable common functions
-source setup_hosts
+source common_functions
 
 usage () {
   echo "USAGE: $0 [-f] [-h] [-i=<input.local>] [-n=<cloud_node_inventory>]"
@@ -74,55 +74,22 @@ done
 inputFile=${INPUT_LOCAL}
 cloudHpcInventory=${CLOUD_HPC_INVENTORY}
 
-if [[ -z "${inputFile}" || ! -e "${inputFile}" ]];then
-  echo "Error: Unable to access local input file -> \"${inputFile}\""
-  exit 1
-else
-  . ${inputFile} || { echo "Error sourcing ${inputFile}"; exit 1; }
-fi
-if [[ -z "${cloudHpcInventory}" || ! -e "${cloudHpcInventory}" ]];then
-  echo "Error: Unable to access Cloud hpc inventory file -> \"${cloudHpcInventory}\""
-  exit 1
-else
-  . ${cloudHpcInventory} || { echo "Error sourcing ${cloudHpcInventory}"; exit 1; }
-fi
-
-_BADCOUNT=0
-
-for((i=0; i<${#c_ip[@]}; i++)) ; do
-  if ! [[ ${c_ip[i]} =~ ^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([\
-                            0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]]; then
-    echo "ERROR: Invalid IP address #$i: ${c_ip[i]}"
-    _BADCOUNT=$((_BADCOUNT+1))
-  fi
-  if ! [[ ${c_bmc[i]} =~ ^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([\
-                             0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]]; then
-    echo "ERROR: Invalid BMC IP address #$i: ${c_bmc[i]}"
-    _BADCOUNT=$((_BADCOUNT+1))
-  fi
-  if ! [[ `echo ${c_mac[i]^^} | egrep "^([0-9A-F]{2}:){5}[0-9A-F]{2}$"` ]]; then
-    echo "ERROR: Invalid MAC address #$i: ${c_mac[i]}"
-    _BADCOUNT=$((_BADCOUNT+1))
-  fi
-done
-
-[[ $_BADCOUNT -eq 0 ]] || exit 3
+validateInputFile
+validateHpcInventory
 
 # -------------------------------- Begin Recipe -------------------------------------------
 # Commands below are extracted from the install guide recipe and are intended for 
 # execution on the master SMS host.
 # -----------------------------------------------------------------------------------------
 
-
-# Determine number of computes and their hostnames
-export num_computes=${num_computes:-${#cc_ip[@]}}
-for((i=0; i<${num_computes}; i++)) ; do
-   cc_name[$i]=${nodename_prefix}$((i+1))
-done
-export c_name
+# Determine number of cloud computes and their hostnames
+setup_computename
 
 #set up hosts at head node or sms node
-setup_hosts
+#setup_hosts
+
+# extend HPC to cloud
+time source set_os_hpc
 
 true
 
