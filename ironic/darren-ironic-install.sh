e0eb97badbf14778fbada7e99607fc8350c1fa18
Darren Peterson-Roulst <darren.peterson-roulst@intel.com>
2016-09-22 09:26:03 -0700

    created new file for ironic installation

---
+++ ironic/darren-ironic-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#---------------------------------------------------------------------------------
+#This script installs and configures ironic for baremetal provisioning on CentOS 7
+#Using the OpenStack-Mitaka release
+#---------------------------------------------------------------------------------
+
+
