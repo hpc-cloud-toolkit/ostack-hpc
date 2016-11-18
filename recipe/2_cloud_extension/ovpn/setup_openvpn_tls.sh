1f2ece89395179cdfbb17c731f75e1275e5aee31
Sunil Mahawar <sunil.mahawar@intel.com>
2016-11-18 17:33:09 -0500

    added setup and configuration directory for openvpn updated README accordingly

---
+++ recipe/2_cloud_extension/ovpn/setup_openvpn_tls.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# This script installs openvpn and easy-rsa, then creates tls key and 
+# certificates for server and client. By default it setup tls security
+# This script is tested on centos7.2, openvpn version 2.3.12 and
+# easy-rsa version 2.0
+yum -y install openvpn
+yum -y install easy-rsa
+#update easy-rsa vars file
+cp -f vars /usr/share/easy-rsa/2.0/
+# create directory for keys
+export openvpn_tls_cert="/etc/openvpn/keys"
+mkdir -p $openvpn_tls_cert
+# copy server configuration file
+cp -f server.conf $openvpn_tls_cert/../
+
+#start the key, certification generation process
+. ./vars
+./clean-all
+
+#generate Certificate Authority
+pushd /usr/share/easy-rsa/2.0/
+./build-ca --batch
+
+# generate certification and private key for the vpn server
+./build-key-server --batch server
+
+#generate diffie hellman pameters
+./build-dh 
+
+#generate client certification and keys
+./build-key --batch cloudhead
+
+# cloudhead* files along with ca.crt shall be copied on cloudhead node
+
+popd
+
+
+
+
+
