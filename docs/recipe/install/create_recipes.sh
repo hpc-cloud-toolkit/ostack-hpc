7e508656ab5258c760a286b784dd6b5a9609a035
James Kling <jkling@localhost.localdomain>
2017-06-02 09:18:17 -0700

    populate all scripts

--- docs/recipe/install/create_recipes.sh
+++ docs/recipe/install/create_recipes.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 
 input_file="./centos7/x86_64/openstack/slurm/steps.tex"
+flavor="3_hpc_as_a_service"
 recipe2="prepare_chpc_image"
 recipe3="prepare_cloud_init"
 recipe4="deploy_chpc_openstack"
 recipe5="prepare_chpc_openstack"
 optional2="update_mrsh"
 optional3="update_clustershell"
-optional4="enable_gender"
-
+optional4="enable_genders"
+support2="README"
+support3="set_os_hpc"
+support4="update_cnodes_to_sms"
 
 
 if [ -e output.txt ]; then
@@ -30,22 +33,31 @@ fi
 
 perl_outfile='output.txt' 
 
-awk 'BEGIN{RS="\n?#   XFILEX"} (NR-1){print $0 > ("recipe_" NR)}' $perl_outfile
+awk 'BEGIN{RS="\n?XFILEX"} (NR-1){print $0 > ("recipe_" NR)}' $perl_outfile
 awk 'BEGIN{RS="\n?ZFILEZ"} (NR-1){print $0 > ("optional_" NR)}' $perl_outfile
-
+awk 'BEGIN{RS="\n?QFILEQ"} (NR-1){print $0 > ("support_" NR)}' $perl_outfile
 
 echo "Parsing to individual recipes"
-mv recipe_2 $recipe2
-mv recipe_3 $recipe3
-mv recipe_4 $recipe4
-mv recipe_5 $recipe5
-if [ ! -e sms ]; then
-	mkdir sms
+
+if [ ! -e $flavor ]; then
+        mkdir $flavor
 fi
-mv optional_2 sms/$optional2
-mv optional_3 sms/$optional3
-mv optional_4 sms/$optional4
+
+mv recipe_2 $flavor/$recipe2
+mv recipe_3 $flavor/$recipe3
+mv recipe_4 $flavor/$recipe4
+mv recipe_5 $flavor/$recipe5
+if [ ! -e $flavor/sms ]; then
+	mkdir $flavor/sms
+fi
+mv optional_2 $flavor/sms/$optional2
+mv optional_3 $flavor/sms/$optional3
+mv optional_4 $flavor/sms/$optional4
+
+mv support_2 $flavor/$support2
+mv support_3 $flavor/$support3
+mv support_4 $flavor/$support4
 
 rm -rf output.txt
 rm -rf optional_*
-
+rm -rf support_*
