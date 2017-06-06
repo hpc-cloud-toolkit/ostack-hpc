bca8888599c1b885b3226bed258614a2d30b1935
James Kling <jkling@localhost.localdomain>
2017-06-05 17:19:51 -0700

    changes called out

--- docs/recipe/install/create_recipes.sh
+++ docs/recipe/install/create_recipes.sh
@@ -5,46 +5,27 @@ flavor1="1_combined_controller"
 flavor2="2_cloud_extension"
 flavor3="3_hpc_as_a_service"
 recipe2="prepare_chpc_image"
-recipe3="prepare_cloud_init"
-recipe4="deploy_chpc_openstack"
-recipe5="prepare_chpc_openstack"
+recipe3="NULL"
+recipe4="prepare_cloud_init"
+recipe5="deploy_chpc_openstack"
+recipe6="prepare_chpc_openstack"
 optional2="update_mrsh"
 optional3="update_clustershell"
 optional4="enable_genders"
+optional5="NULL"
 support2="README"
 support3="set_os_hpc"
 support4="update_cnodes_to_sms"
 heat2="heat-sms.yaml"
 heat3="heat-cn.yaml"
-parent2="hera_preq/15-default.conf"
-parent3="hera_preq/cmt_ohpc_inputs_dummy"
-parent4="hera_preq/CentOS-Base.repo"
-parent5="hera_preq/setup.sh"
-parent6="hera_preq/environment"
-parent7="common_functions"
-parent8="c_init_workaround"
-parent9="setup_cloud_hpc.sh"
-parent10="hpc_cent7/intel/recipe.sh"
-parent11="hpc_cent7/intel/sections/sec5-sec6:Resource_Manager_Startup.sh"
-parent12="hpc_cent7/intel/sections/sec3.8-sec3.8.3:Define_compute_image_for_provisioning.sh"
-parent13="hpc_cent7/intel/sections/sec3.8.4-sec3.8.4.11:Additional_Customizations_-optional-.sh"
-parent14="hpc_cent7/intel/sections/sec3.1-sec3.2:Enable_required_repositories.sh"
-parent15="hpc_cent7/intel/sections/sec3.4-sec3.7:Initial_HeadNode_configuration.sh"
-parent16="hpc_cent7/intel/sections/sec7:CLCK_Supportability_Extensions.sh"
-parent17="hpc_cent7/intel/sections/sec3.9-sec3.10:Finalize_Provisioning.sh"
-parent18="hpc_cent7/intel/sections/sec4.1-sec4.7:Install_Development_Components.sh"
-parent19="hpc_cent7/intel/input.local"
-parent20="hpc_cent7/intel/orch_bug_wr"
-parent21="hpc_cent7/intel/orch.conf"
-parent22="hpc_cent7/ohpc/recipe.sh"
-parent23="hpc_cent7/ohpc/ohpc.conf"
-parent24="hpc_cent7/ohpc/input.local"
-parent25="hpc_cent7/ohpc/ohpc_sun_hn2.conf"
-parent26="hpc_cent7/ohpc/ohpc_sun_hn3.conf"
-parent27="teardown_cloud_nodes.sh"
-parent28="get_cn_mac"
-parent29="cloud_hpc_init/orch/chpc_init"
-parent30="cloud_hpc_init/ohpc/chpc_init"
+parent2="chpc_init"
+parent3="chpc_sms_init"
+parent4="NULL"
+parent5="common_functions"
+parent6="NULL"
+parent7="setup_cloud_hpc.sh"
+parent8="teardown_cloud_nodes.sh"
+parent9="get_cn_mac"
 
 
 echo "There are three types of recipes, but at present only '3 - HPC as a Service' is useable."
@@ -81,9 +62,9 @@ mkdir -p ./recipe/$flavor3/sms
 mkdir -p ./recipe/$flavor3/heat_templates
 
 mv recipe_2 ./recipe/$flavor3/$recipe2
-mv recipe_3 ./recipe/$flavor3/$recipe3
 mv recipe_4 ./recipe/$flavor3/$recipe4
 mv recipe_5 ./recipe/$flavor3/$recipe5
+mv recipe_6 ./recipe/$flavor3/$recipe6
 
 if [ ! -e ./recipe/$flavor3/sms ]; then
 	mkdir ./recipe$flavor3/sms
@@ -104,26 +85,19 @@ mkdir -p ./recipe/cloud_hpc_init/ohpc
 # ^^ Has 2 files
 mkdir -p ./recipe/cloud_hpc_init/orch
 # ^^ Has 1 file
-mkdir -p ./recipe/hpc_cent7/ohpc
-# ^^ Has 5 files
-mkdir -p ./recipe/hpc_cent7/intel/sections
-# ^^ Has 4 files in 'intel', and 8 files in 'sections'
-mkdir -p ./recipe/inventory/$flavor1
-# ^^ TBC later
-mkdir -p ./recipe/inventory/$flavor2
-# ^^ TBC Later
-mkdir -p ./recipe/inventory/$flavor3
-# ^^ Has 7 files, 2 subdirs: 'heat_templates' with 2 files, 'sms' with three
-mkdir -p ./recipe/hera_preq
-# ^^ Has 5 files
 
-for i in `seq 2 30`; do 
+mkdir -p recipe/cloud_hpc_init/ohpc
+mv parent_2 recipe/cloud_hpc_init/ohpc/chpc_init
+mv parent_3 recipe/cloud_hpc_init/ohpc/chpc_sms_init
+for i in 5 7 8 9; do 
 	str="parent$i"
 	mv parent_$i ./recipe/${!str}
 done
 
+
 rm -rf output.txt
 rm -rf optional_*
 rm -rf support_*
+rm -rf recipe_*
 rm -rf heat_*
 rm -rf parent_*
