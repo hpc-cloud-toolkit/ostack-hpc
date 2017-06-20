38ea90c6526e3935289f0eadae10081b6aafc324
Sunil Mahawar <sunil.mahawar@intel.com>
2017-06-20 16:20:56 -0400

    added additional tests from ohpc

---
+++ tests/libs/boost/tests/accumulators/test/max.cpp
@@ -0,0 +1,37 @@
+//  (C) Copyright Eric Niebler 2005.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#define BOOST_TEST_MODULE accumulators
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+
+using namespace boost;
+using namespace unit_test;
+using namespace accumulators;
+
+///////////////////////////////////////////////////////////////////////////////
+// test_stat
+//
+void test_stat()
+{
+    accumulator_set<int, stats<tag::max> > acc;
+
+    acc(1);
+    BOOST_CHECK_EQUAL(1, (max)(acc));
+
+    acc(0);
+    BOOST_CHECK_EQUAL(1, (max)(acc));
+
+    acc(2);
+    BOOST_CHECK_EQUAL(2, (max)(acc));
+}
+
+BOOST_AUTO_TEST_CASE( max_accumulator )
+{
+   test_stat();
+}
